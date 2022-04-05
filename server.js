6f886f5f5d3c30234fad6a0dec0c2a5781505fad
Glitch (hello-express) <none>
2022-04-05 15:03:33 +0000

    😻🐞 Checkpoint ./package.json:35147739/1484 ./server.js:35147739/1462 ./views/index.html:35147739/27

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+//envファイルからmongoDB接続
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const mongouri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.MONGOHOST;
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
 
 app.use(express.static("public"));
+// listen for requests :)
 
 
-
-// https://expressjs.com/en/starter/basic-routing.html
+// 最初のページを表示
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/src/pages/index.html");
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+//ページ遷移
+app.get("/input", (request, response) => {
+  response.sendFile(__dirname + "/views/directuser.html");
+});
+
+app.get("/input2", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
 });
