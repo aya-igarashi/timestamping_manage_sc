652c8b10d164a67807a507ec9510907950e46462
Glitch (hello-express) <none>
2022-04-06 13:42:53 +0000

    👮🐥 Checkpoint ./server.js:35147739/1332 ./model.js:35147739/947

--- model.js
+++ model.js
@@ -1,6 +1,17 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const mongouri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.MONGOHOST;
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+var db = MongoClient.connect(mongouri, function(err, res) {
+   if(err){
+        console.log('Error connected: ' + mongouri + ' - ' + err);
+    }else{
+        console.log('Success connected: ' + mongouri);
+    }
 });
+
+var UserSchema = new mongodb.Schema({
+    email    : String,
+    password  : String
+},{collection: 'users'});
+
+exports.User = db.model('timestamping_system_DB', UserSchema);
