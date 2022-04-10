2a4a870bf5774b560e75d53274c0165d63ab9e94
Glitch (hello-express) <none>
2022-04-10 09:31:11 +0000

    🌛🍵 Checkpoint ./public/user_info.js:35147739/51 ./views/user_info.html:35147739/320 ./views/directuser.html:35147739/45

--- public/user_info.js
+++ public/user_info.js
@@ -3,7 +3,7 @@ document.getElementById("tosearch").onclick = function(){
     const search_day = document.getElementById("search_day").value;
     const tbody = document.getElementById('tbody'); 
   
-    while(tbody.firstChild) tbody.removeChild(tbody.firstChild);    
+    //while(tbody.firstChild) tbody.removeChild(tbody.firstChild);    
   
     const url = '/find'; // 通信先
     const req = new XMLHttpRequest(); // 通信用オブジェクト
@@ -16,26 +16,26 @@ document.getElementById("tosearch").onclick = function(){
     req.onreadystatechange = function(){
       if(req.readyState == 4 && req.status == 200) {
         console.log(req.response);
-        const results = JSON.parse(req.response);
-           
-        for(let i in results) {
-          const tr = document.createElement('tr');
-          const td_type = document.createElement('td');
-          td_type.innerHTML = results[i].theme;
-          const td_time = document.createElement('td');
-          td_time.innerHTML = results[i].question; 
-          const jbBtn = document.createElement( 'button' );
-          const jbBtnText = document.createTextNode( '削除' );
-          jbBtn.appendChild( jbBtnText );
-          jbBtn.name = 'delete_btn';
-          jbBtn.value = results[i]._id;
-          jbBtn.setAttribute('onclick', 'remove("' + results[i]._id + '")');
-        
-          tr.appendChild(td_type);
-          tr.appendChild(td_time);
-          tr.appendChild(jbBtn);
-          tbody.appendChild(tr);
-        }
+//        const results = JSON.parse(req.response);
+//          
+//        for(let i in results) {
+//          const tr = document.createElement('tr');
+//          const td_type = document.createElement('td');
+//          td_type.innerHTML = results[i].theme;
+//          const td_time = document.createElement('td');
+//          td_time.innerHTML = results[i].question; 
+//          const jbBtn = document.createElement( 'button' );
+//          const jbBtnText = document.createTextNode( '削除' );
+//          jbBtn.appendChild( jbBtnText );
+//          jbBtn.name = 'delete_btn';
+//          jbBtn.value = results[i]._id;
+//          jbBtn.setAttribute('onclick', 'remove("' + results[i]._id + '")');
+//        
+//          tr.appendChild(td_type);
+//          tr.appendChild(td_time);
+//          tr.appendChild(jbBtn);
+//          tbody.appendChild(tr);
+//        }
         
       }
     }
